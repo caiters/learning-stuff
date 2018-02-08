d4585c449f157a93f101235da0c5b43940b758b9
Caitlin <caitlin.bailey@webadvanced.com>
2018-02-08 13:58:22 -0800

    cat clicker pro and also refactoring a little bit to make sure view never directly touches model even just to retrieve data

--- cat-clicker-premium/app.js
+++ cat-clicker-premium/app.js
@@ -27,12 +27,24 @@ const model = {
     ],
     addClick(id) {
         this.cats[id].clicks++;
+    },
+    getCat(id) {
+        return { ...this.cats[id] }
+    },
+    getAllCats() {
+        return [ ...this.cats ]
+    },
+    saveCat(catData, id) {
+        this.cats[id] = catData;
     }
 };
 
 const octopus = {
     init() {
-        catListView.init();
+        const cats = model.getAllCats();
+        catListView.loadCatListView(cats);
+        adminView.init();
+        this.selectCat(0); // load first cat by default
     },
     getClicks(id) {
         return model.cats[id].clicks;
@@ -41,15 +53,54 @@ const octopus = {
         model.addClick(id);
         const numberClicks = this.getClicks(id);
         catDetailView.updateClicks(numberClicks);
+        adminView.updateClicks(numberClicks);
     },
     selectCat(id) {
-        catDetailView.load(id);
+        const cat = model.getCat(id);
+        catDetailView.load(cat, id);
+        adminView.load(cat, id);
+    },
+    saveCat(catData, id) {
+        model.saveCat(catData, id);
+        catListView.loadCatListView(model.cats);
+        catDetailView.load(model.getCat(id), id);
     }
 };
 
-const catListView = {
+const adminView = {
     init() {
-        document.querySelector('#catsList').innerHTML = this.buildListHtml();
+        document.querySelector('#admin').classList.remove('admin-wrapper--hidden');
+        document.querySelector('#adminBtn').addEventListener('click', this.toggleWindow);
+        document.querySelector('#adminCancel').addEventListener('click', this.toggleWindow);
+    },
+    toggleWindow() {
+        document.querySelector('.admin-window').classList.toggle('admin-window--hidden')
+    },
+    load(cat, id) {
+        document.querySelector('#name').value = cat.name;
+        document.querySelector('#image').value = cat.photo;
+        document.querySelector('#clicks').value = cat.clicks;
+        document.querySelector('#adminSubmit').dataset.catId = id;
+        document.querySelector('#adminSubmit').addEventListener('click', this.saveCat);
+    },
+    updateClicks(num){
+        document.querySelector('#clicks').value = num;
+    },
+    saveCat(e) {
+        const id = e.target.dataset.catId;
+        const catData = {
+            name: document.querySelector('#name').value,
+            photo: document.querySelector('#image').value,
+            clicks: document.querySelector('#clicks').value,
+        };
+        octopus.saveCat(catData, id);
+        adminView.toggleWindow();
+    }
+}
+
+const catListView = {
+    loadCatListView(cats) {
+        document.querySelector('#catsList').innerHTML = this.buildListHtml(cats);
 
         const catLinks = document.querySelectorAll('#catsList a');
         catLinks.forEach(function(link){
@@ -59,9 +110,10 @@ const catListView = {
             });
         });
     },
-    buildListHtml() {
+    buildListHtml(cats) {
+        console.log(cats);
         let listHtml = '';
-        model.cats.forEach(function(cat, i) {
+        cats.forEach(function(cat, i) {
             listHtml += `
             <li>
                 <a href="#" data-id="${i}">
@@ -75,25 +127,24 @@ const catListView = {
 }
 
 const catDetailView = {
-    load(id) {
-        const cat = document.querySelector('#cat');
-        cat.innerHTML = this.buildCatHtml(id);
+    catDiv: document.querySelector('#cat'),
+    load(cat, id) {
+        this.catDiv.innerHTML = this.buildCatHtml(cat, id);
         const catImg = document.querySelector('img.cat');
-        const catClickCounter = document.querySelector('span.cat-clicks');
         catImg.addEventListener('click', function(e){
             octopus.catClick(e.target.dataset.id)
         });
     },
-    buildCatHtml(id) {
+    buildCatHtml(cat, id) {
         return `
-            <p>${model.cats[id].name}</p>
-            <img class="cat" data-id="${id}" src="img/${model.cats[id].photo}" alt="cat" />
-            <p>Clicks: <span class="cat-clicks" data-id="${id}">${model.cats[id].clicks}</span></p>
+            <p>${cat.name}</p>
+            <img class="cat" data-id="${id}" src="img/${cat.photo}" alt="cat" />
+            <p>Clicks: <span class="cat-clicks" data-id="${id}">${cat.clicks}</span></p>
         `;
     },
     updateClicks(num) {
-        const catClicks = document.querySelector('#cat .cat-clicks');
-        catClicks.innerHTML = num;
+        const catClickCounter = document.querySelector('span.cat-clicks');
+        catClickCounter.innerHTML = num;
     }
 }
 
